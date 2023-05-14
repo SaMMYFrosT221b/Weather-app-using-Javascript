a7fbba5a38972306f0e364bc835d1152275065e7
SaMMYFrosT <sammy.frost.221b@gmail.com>
2023-05-15 00:47:34 +0530

    Checking python is running in html or not

--- city.py
+++ city.py
@@ -1,14 +1,63 @@
-import requests
 import os
-os.system('cls')
 
-response = requests.get("http://dataservice.accuweather.com/locations/v1/cities/search?apikey=hX1bnPZXuV3Pi7AVX6WdmOQ89sWMJrq7&q=delhi")
+import requests
 
-print(response.json())
+os.system("cls")
 
+# response = requests.get("http://dataservice.accuweather.com/locations/v1/cities/search?apikey=hX1bnPZXuV3Pi7AVX6WdmOQ89sWMJrq7&q=delhi")
 
+DATA = {
+    "Version": 1,
+    "Key": "202396",
+    "Type": "City",
+    "Rank": 11,
+    "LocalizedName": "Delhi",
+    "EnglishName": "Delhi",
+    "PrimaryPostalCode": "",
+    "Region": {"ID": "ASI", "LocalizedName": "Asia", "EnglishName": "Asia"},
+    "Country": {"ID": "IN", "LocalizedName": "India", "EnglishName": "India"},
+    "AdministrativeArea": {
+        "ID": "DL",
+        "LocalizedName": "Delhi",
+        "EnglishName": "Delhi",
+        "Level": 1,
+        "LocalizedType": "Union Territory",
+        "EnglishType": "Union Territory",
+        "CountryID": "IN",
+    },
+    "TimeZone": {
+        "Code": "IST",
+        "Name": "Asia/Kolkata",
+        "GmtOffset": 5.5,
+        "IsDaylightSaving": False,
+        "NextOffsetChange": None,
+    },
+    "GeoPosition": {
+        "Latitude": 28.643,
+        "Longitude": 77.118,
+        "Elevation": {
+            "Metric": {"Value": 215.0, "Unit": "m", "UnitType": 5},
+            "Imperial": {"Value": 705.0, "Unit": "ft", "UnitType": 0},
+        },
+    },
+    "IsAlias": False,
+    "SupplementalAdminAreas": [
+        {"Level": 2, "LocalizedName": "West", "EnglishName": "West"},
+        {
+            "Level": 3,
+            "LocalizedName": "Rajouri Garden",
+            "EnglishName": "Rajouri Garden",
+        },
+    ],
+    "DataSets": [
+        "AirQualityCurrentConditions",
+        "AirQualityForecasts",
+        "Alerts",
+        "ForecastConfidence",
+        "FutureRadar",
+        "MinuteCast",
+        "PremiumAirQuality",
+    ],
+}
 
-
-
-
-
+print(DATA.keys())
--- main.py
+++ main.py
@@ -3,7 +3,9 @@ import os
 os.system('cls')
 # response  = requests.get("http://dataservice.accuweather.com/forecasts/v1/daily/1day/202396?apikey=hX1bnPZXuV3Pi7AVX6WdmOQ89sWMJrq7")
 
-DATA = {'Headline': {'EffectiveDate': '2023-05-15T07:00:00+05:30', 'EffectiveEpochDate': 1684114200, 'Severity': 4, 'Text': 'Danger of dehydration and heat stroke if outside for extended periods of time Monday', 'Category': 'heat', 'EndDate': '2023-05-15T19:00:00+05:30', 'EndEpochDate': 1684157400, 'MobileLink': 'http://www.accuweather.com/en/in/delhi/202396/daily-weather-forecast/202396?lang=en-us', 'Link': 'http://www.accuweather.com/en/in/delhi/202396/daily-weather-forecast/202396?lang=en-us'}, 'DailyForecasts': [{'Date': '2023-05-14T07:00:00+05:30', 'EpochDate': 1684027800, 'Temperature': {'Minimum': {'Value': 81.0, 'Unit': 'F', 'UnitType': 18}, 'Maximum': {'Value': 104.0, 'Unit': 'F', 'UnitType': 18}}, 'Day': {'Icon': 5, 'IconPhrase': 'Hazy sunshine', 'HasPrecipitation': False}, 'Night': {'Icon': 37, 'IconPhrase': 'Hazy moonlight', 'HasPrecipitation': False}, 'Sources': ['AccuWeather'], 'MobileLink': 'http://www.accuweather.com/en/in/delhi/202396/daily-weather-forecast/202396?day=1&lang=en-us', 'Link': 'http://www.accuweather.com/en/in/delhi/202396/daily-weather-forecast/202396?day=1&lang=en-us'}]}
+DATA = {'Headline': {'EffectiveDate': '2023-05-15T07:00:00+05:30', 'EffectiveEpochDate': 1684114200, 'Severity': 4, 'Text': 'Danger of dehydration and heat stroke if outside for extended periods of time Monday', 'Category': 'heat', 'EndDate': '2023-05-15T19:00:00+05:30', 'EndEpochDate': 1684157400, 'MobileLink': 'http://www.accuweather.com/en/in/delhi/202396/daily-weather-forecast/202396?lang=en-us', 'Link': 'http://www.accuweather.com/en/in/delhi/202396/daily-weather-forecast/202396?lang=en-us'}, 'DailyForecasts': [
+    {'Date': '2023-05-14T07:00:00+05:30', 'EpochDate': 1684027800, 'Temperature': {'Minimum': {'Value': 81.0, 'Unit': 'F', 'UnitType': 18}, 'Maximum': {'Value': 104.0, 'Unit': 'F', 'UnitType': 18}}, 'Day': {'Icon': 5, 'IconPhrase': 'Hazy sunshine', 'HasPrecipitation': False}, 'Night': {'Icon': 37, 'IconPhrase': 'Hazy moonlight', 'HasPrecipitation': False}, 'Sources': ['AccuWeather'], 'MobileLink': 'http://www.accuweather.com/en/in/delhi/202396/daily-weather-forecast/202396?day=1&lang=en-us', 'Link': 'http://www.accuweather.com/en/in/delhi/202396/daily-weather-forecast/202396?day=1&lang=en-us'}]}
+
 
 # print(DATA.keys())
 # print(" ")
@@ -20,4 +22,3 @@ print(DATA['DailyForecasts'][0]['Night'])
 print(DATA['DailyForecasts'][0]['Sources'])
 print(DATA['DailyForecasts'][0]['MobileLink'])
 print(DATA['DailyForecasts'][0]['Link'])
-
